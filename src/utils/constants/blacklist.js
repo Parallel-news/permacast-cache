1e602d3c629b557cbf6e61d36cc68f05cc3852f6
Darwin <77340894+charmful0x@users.noreply.github.com>
2021-12-16 00:48:16 +0200

    fix: All Core Devs Meetings

--- src/utils/constants/blacklist.js
+++ src/utils/constants/blacklist.js
@@ -3,7 +3,7 @@
 
 export const BLACKLIST = {
 	podcasts : [],
-	episodes: ["qAKLFkO_PZwcXzvt9naoZ3KzatTqxrxWXc7kk8LcSJI", "iJUt4Pn8EbC-vkcCiHKGzgQuoSVHa-eEzvb4QqDgxy4"
+	episodes: ["qAKLFkO_PZwcXzvt9naoZ3KzatTqxrxWXc7kk8LcSJI", "iJUt4Pn8EbC-vkcCiHKGzgQuoSVHa-eEzvb4QqDgxy4",
 		  "DPQ0kIN_mf_YCBvmxaaYk9YURdQL90w6jTSlOwbyeIQ"],
 };
 
