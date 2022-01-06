738c9bd41ac6e3381965a0baca644c1cbd4cb82f
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-01-06 14:10:34 +0200

    feat: blacklist podcasts (filtering)

--- src/utils/smartweave.js
+++ src/utils/smartweave.js
@@ -1,4 +1,5 @@
 import { smartweave, arweave } from "./arweave.js";
+import { BLACKLIST } from "./constants/blacklist.js";
 import { readContract } from "smartweave";
 import { gqlTemplate, permacastDeepGraphs } from "./gql.js";
 import base64url from "base64url";
@@ -18,11 +19,29 @@ export async function getFactoriesObjects() {
   return factories;
 }
 
+async function blacklistFactoryPodcast(state) {
+  // blacklist a podcast based on its pid
+  // remove the podcast object from the factory's
+  // state and return the new state if blacklist
+  // was found
+  const blacklistedPodcasts = BLACKLIST.podcasts;
+  const blacklistedPodcastIndex = state.podcasts.findIndex((podObj) =>
+    blacklistedPodcasts.includes(podObj.pid)
+  );
+  if (blacklistedPodcastIndex !== -1) {
+    state.podcasts.splice(blacklistedPodcastIndex, 1);
+    return state;
+  }
+
+  return state;
+}
+
 export async function getFactoriesState() {
   const factoriesObj = await getFactoriesObjects();
   const states = [];
   for (let factory of factoriesObj) {
-    const state = await getStateOf(factory.id);
+    const unfiliteredState = await getStateOf(factory.id);
+    const state = await blacklistFactoryPodcast(unfiliteredState);
     // set factory metadata
     state.factory_id = factory.id;
     state.owner = factory.owner;
