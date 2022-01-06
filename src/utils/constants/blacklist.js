dcddc66e65a355070f6b242d242599ea1a68a440
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-01-06 17:37:19 +0200

    add:  MesonNetwork episodes duplication

--- src/utils/constants/blacklist.js
+++ src/utils/constants/blacklist.js
@@ -5,6 +5,7 @@ export const BLACKLIST = {
 	podcasts : ["JIixQWc-Mq6Y6PTPWm32s3qfCEPi3VVmyACMd59lOhg"],
 	episodes: ["qAKLFkO_PZwcXzvt9naoZ3KzatTqxrxWXc7kk8LcSJI", "iJUt4Pn8EbC-vkcCiHKGzgQuoSVHa-eEzvb4QqDgxy4",
 		  "DPQ0kIN_mf_YCBvmxaaYk9YURdQL90w6jTSlOwbyeIQ", "djJEN3GqtMXxJacYHFY5PsYRBPtjq6SeUjLs7ofga7I",
-		  "leSBDC4YQej8JnzJG--wQZ1mKxGQQnIs8_sA1W5wa_w", "5jIdVke2AL_lHLlvShjeGlChQdIKkum1yWUf_Oihsgc"],
+		  "leSBDC4YQej8JnzJG--wQZ1mKxGQQnIs8_sA1W5wa_w", "5jIdVke2AL_lHLlvShjeGlChQdIKkum1yWUf_Oihsgc",
+		  "jf5txlYQFc7gEjxa7Sb0Zw7uibTDuG739oOmB5ghQI4", "TASc9QBPCUuInpLwk-EByqHn1LzRawBGytjSuk_Z3n4"],
 };
 
