f7890b1a0165918a8bdb38c539d2c726656af390
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-02-23 20:59:16 +0200

    feat: embed episodes

---
+++ src/embed/embeding.js
@@ -0,0 +1,41 @@
+import { getPodcasts } from "../utils/cache.js";
+import { defaultEmbed } from "../utils/constants/defaultEmbed.js";
+import base64url from "base64url";
+
+export async function getEpisodeMetadata(eid) {
+  try {
+    const encodedState = await getPodcasts();
+    const state = JSON.parse(base64url.decode(encodedState))?.res;
+
+    if (!state) {
+      return defaultEmbed;
+    }
+
+    const existenceOfEpParent = state.findIndex((pod) =>
+      pod.episodes.find((ep) => ep["eid"] === eid)
+    );
+
+    if (existenceOfEpParent === -1) {
+      return defaultEmbed;
+    }
+
+    const episodeIndexInParent = state[existenceOfEpParent].episodes.findIndex(
+      (ep) => ep["eid"] === eid
+    );
+
+    const podcast = state[existenceOfEpParent];
+    const episode = podcast["episodes"][episodeIndexInParent];
+
+    const episodeMetadata = {
+      episode_name: episode.episodeName,
+      episode_url: `https://arweave.net/${episode.audioTx}`,
+      episode_desc: episode.description,
+      episode_podcast: podcast.podcastName,
+      podcast_cover: `https://arweave.net/${podcast.cover}`,
+    };
+
+    return episodeMetadata;
+  } catch (error) {
+    console.log(`${error.name} : ${error.description}`);
+  }
+}
