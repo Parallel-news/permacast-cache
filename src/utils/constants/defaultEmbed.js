81781fe5fd7f97688151a5f0a33252e605d82177
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-02-23 20:58:12 +0200

    feat: embed episodes

---
+++ src/utils/constants/defaultEmbed.js
@@ -0,0 +1,7 @@
+export const defaultEmbed = {
+  episode_name: "All Core Devs: Meeting 9",
+  episode_url: `https://arweave.net/m5BTx0-pGmdJJPlL3hqOJodE3aUoOa61vOpjxUZQUqA`,
+  episode_desc: `1/25/2017 14:00PM UTC`,
+  episode_podcast: `All Core Devs Meetings`,
+  podcast_cover: `https://arweave.net/yrpUugY1p6kzo_0KgAdtV9dronn6T_2gh_IpAykHuMc`,
+};
