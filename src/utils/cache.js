a6f20411c93c590fdc93dc453f07c476b046f463
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-03-10 18:52:06 +0200

    fix: MASKING_CONTRACT access

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -25,7 +25,7 @@ function sleep(ms) {
 
 async function removeBlacklists(podObj) {
   const episodes = podObj["episodes"];
-  let BLACKLISTED_EPISODES = await getStateOf(MASKED);
+  let BLACKLISTED_EPISODES = await getStateOf(MASKING_CONTRACT);
   
   if (!BLACKLISTED_EPISODES) {
     BLACKLISTED_EPISODES = BLACKLIST;
