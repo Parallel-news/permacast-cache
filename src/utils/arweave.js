650edf6315a2a072bc6f4c31d5434468142706ce
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-05-19 19:59:36 +0300

    perf:  add readContract function

--- src/utils/arweave.js
+++ src/utils/arweave.js
@@ -1,17 +1,24 @@
 import Arweave from "arweave";
-import { SmartWeaveNodeFactory } from "redstone-smartweave";
+import { SmartWeaveNodeFactory, LoggerFactory } from "redstone-smartweave";
 
 export const arweave = Arweave.init({
   host: "arweave.net",
   port: 443,
   protocol: "https",
-  timeout: 20000,
+  timeout: 60000,
   logging: false,
 });
 
-export const smartweave = SmartWeaveNodeFactory.memCached(arweave);
+const smartweave = SmartWeaveNodeFactory.memCached(arweave);
 
+export async function readContract(swc_id) {
+  try {
+    const contract = smartweave.contract(swc_id);
+    const res = await contract.readState();
 
-
-
+    return res.state;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
